e627b5b6d9fe9432914c3d626d3bf344a0f83d32
Igor Marinho <86384484+Igor-Marinho-Ferreira@users.noreply.github.com>
2022-06-19 18:39:26 -0300

    ATT - BY IGOR

--- ProjetoC.ino
+++ ProjetoC.ino
@@ -23,6 +23,7 @@
 
 Adafruit_SSD1306 display = Adafruit_SSD1306();
 char buff[40];
+char buff1[40];
 struct Senha{ //strutura de senhas
   int id; // id da senha, serve para identicar a senha
   int senha; // a senha propriamente dita
@@ -36,6 +37,10 @@ struct SenhasChamadas{ //strutura para guardar as senhas já chamadas
 };
 
 Senha* inicializaSenha(){
+  return NULL;
+}
+
+SenhasChamadas* inicializaSenhasChamadas(){
   return NULL;
 }
 
@@ -54,6 +59,7 @@ void setup(){
 void loop()
 {
   Senha* senhas = inicializaSenha(); 
+  SenhasChamadas* senhasChamadas = inicializaSenhasChamadas();
   int cp = 2; 
   int qtdPrioridade = 0; 
   int qtdNormal = 0; 
@@ -75,6 +81,14 @@ void loop()
     delay(1500); //INTERVALO DE 1,5 SEGUNDOS
     display.clearDisplay(); //LIMPA AS INFORMAÇÕES DO DISPLAY
   }
+  if(digitalRead(2) == 1){
+    display.setCursor(0,0); //POSIÇÃO EM QUE O CURSOR IRÁ FAZER A ESCRITA
+    chamar(&senhas, &senhasChamadas, &cp);
+    display.print(buff1); //ESCREVE O TEXTO NO DISPLAY
+    display.display(); //EFETIVA A ESCRITA NO DISPLAY
+    delay(1500); //INTERVALO DE 1,5 SEGUNDOS
+    display.clearDisplay(); //LIMPA AS INFORMAÇÕES DO DISPLAY
+  }  
 }
 
 void gerarSenha(Senha **senha, int qtdNormal, int qtdPrioridade, int prioridade){
@@ -107,6 +121,57 @@ Senha* prioridade(Senha *senhas){
   }
   if (s == NULL) return NULL; 
   return s; 
+}
+
+Senha* excluirsenha(Senha *senhas, int id){
+  Senha* ant = NULL; 
+  Senha* s = senhas; 
+
+  while (s != NULL && s->id != id) { // criamos um looping para percorrer as senhas até o final da fila ou encontarar uma com o id que querermos
+    ant = s; 
+    s = s->next; 
+  }
+  if(s == NULL){
+    return senhas; 
+  }
+  if (ant == NULL) { // se a senha anterior for NULL quer dizer que o que procuramos está no topo da fila
+    senhas = s->next; 
+  }else { 
+    ant->next = s->next; 
+  }
+  free(s); // liberamos da memoria o ponteiro que criamos para percorrer as senhas
+  return senhas;
+}
+
+void chamar(Senha** senhas, SenhasChamadas** senhasChamadas, int *cp){
+
+  if(*senhas == NULL) return; // se a fila de senhas for NULL então a fila está vazia e encerramos a função
+
+  Senha* s; // criamos um ponteiro para senhas
+  if(*cp == 2){
+    s = *senhas;
+    *cp = 0; 
+  }else{ 
+    Senha* prioridades = prioridade(*senhas); // criamos um pontiro para senhas e inicializamos ele com a função de prioridade
+    if(prioridades != NULL){ 
+      s = prioridades; // então atrinuimos ela ao ponteiro de senhas que criamos e incrementamos o contador de senha com prioridades chamadas
+      *cp = *cp + 1; 
+    }else{ 
+      s = *senhas; // então chamamos o proximo da fila, que sempre sera o primeiro
+    }
+  }
+
+  SenhasChamadas* sc = (SenhasChamadas*) malloc(sizeof(SenhasChamadas)); // criamos um ponteiro para as senhas já chamadas e inicializamos alocando memoria 
+  Senha *ant = (Senha*) malloc(sizeof(Senha));  // criamos um ponteiro para uma senha e alocamos memoria do tamannho de uma strutura Senha
+  ant->id = s->id; 
+  ant->prioridade = s->prioridade; 
+  ant->senha = s->senha; 
+  ant->next = NULL; 
+  sc->senha = ant; 
+  sc->next = *senhasChamadas; 
+  *senhasChamadas = sc; 
+  sprintf(buff1,"\nSENHA CHAMADA: %s%d\nTIPO: %s\n", s->prioridade ? " P" : " N", s->senha, s->prioridade ? " PRIORITARIA" : " NORMAL");
+  *senhas = excluirsenha(*senhas, s->id); 
 }
 
 
